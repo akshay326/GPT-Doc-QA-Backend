b4556fac1922352056cd75619f0f031bd88f61c0
Akshay Sharma <static.akshay@gmail.com>
2023-06-20 00:25:41 +0000

    refactor code

--- andes/__init__.py
+++ andes/__init__.py
@@ -1,11 +1,17 @@
 import os
+import time
+import json
+import logging
 from dotenv import load_dotenv
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
 from .models import database as db
 
 # Load the .env file
 load_dotenv()
+
+# set logging level
+logging.getLogger().setLevel(logging.INFO)
 
 UPLOAD_DIRECTORY = os.getenv('UPLOAD_DIRECTORY')
 DB_PATH = os.getenv('DB_PATH')
@@ -29,5 +35,28 @@ def create_app():
     with app.app_context():
         db.init_app(app)
         db.create_all()
+    
+    @app.before_request
+    def start_timer():
+        request.start_time = time.time()
+
+    @app.after_request
+    def log_request_info(response):
+        # Calculate request duration to 3 decimal places
+        request_duration = round(time.time() - request.start_time, 3)
+
+        # Log the message
+        message = {
+            'message': 'API REQUEST',
+            'client_address': request.remote_addr,
+            'requested_url': request.url,
+            'method': request.method,
+            'path': request.path,
+            'status': response.status,
+            'duration': request_duration
+        }
+        logging.info(json.dumps(message))
+
+        return response
 
     return app
